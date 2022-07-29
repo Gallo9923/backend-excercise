76216252a0eae4ac57e567f3ed507e63d496f959
Christian Gallo <42328477+Gallo9923@users.noreply.github.com>
2022-07-29 15:11:02 -0500

    Add discount test

--- tests/store/checkout/test_discount.py
+++ tests/store/checkout/test_discount.py
@@ -1,6 +1,16 @@
 import unittest
 
+from store.checkout.discount import Discount
 class DiscountTestCase(unittest.TestCase):
 
     def test_discount(self):
-        pass
+        name: str = "Voucher Discount"
+        discount: float = 0.0
+        d: Discount = Discount(name=name, discount=discount)
+
+        self.assertEqual(d.name, name)
+        self.assertEqual(d.discount, discount)
+
+
+if __name__ == '__main__':
+    unittest.main()
