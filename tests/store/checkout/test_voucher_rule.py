4d2b69bf92db45daf0d41d8704052a46d36a169a
Christian Gallo <42328477+Gallo9923@users.noreply.github.com>
2022-07-30 00:59:31 -0500

    Add voucher rule tests

--- tests/store/checkout/test_voucher_rule.py
+++ tests/store/checkout/test_voucher_rule.py
@@ -1,7 +1,10 @@
+from dis import dis
 import unittest
+from typing import List
+from store.checkout.discount import Discount
 
 from store.checkout.pricing_rule import VoucherRule
-from store.checkout.product import Product
+from store.checkout.product import Product, Code
 
 class VoucherRuleTestCase(unittest.TestCase):
 
@@ -15,22 +18,59 @@ class VoucherRuleTestCase(unittest.TestCase):
         self.vr = None
     
     def test_VoucherRule(self) -> None:
-        """Test if the the voucher rule is created successfully"""
+        """Tests if the the voucher rule is created successfully"""
+
         self.assertEqual(self.vr.name, self.vr_name)
         self.assertEqual(self.vr.voucher_counter, 0)
 
+    def test_zero_voucher_items(self) -> None:
+        """Tests that no discount is applied with zero voucher items"""
+
+        p: Product(code=Code.PANTS, name="Summer Pants", price=7.5)
+        self.vr.scan(p)
+
+        self.assertEqual(self.vr.get_discount(), None)
+        
+
+    def voucher_list_generator(self, q: int) -> List[Product]:
+        """Generates  a list of q voucher products"""
+
+        voucher_list: List[Product] = []
+        for i in range(q):
+            p: Product = Product(code=Code.VOUCHER, name="Gift Card", price=5.0)
+            voucher_list.append(p)
+        return voucher_list
+
     def test_one_voucher_items(self) -> None:
-        """Test if the voucher discount is applied for one voucher items"""
-        pass
+        """Tests if the voucher discount is applied for one voucher items"""
+        
+        products: List[Product] = self.voucher_list_generator(1)
+        for p in products:
+            self.vr.scan()
+
+        self.assertEqual(self.vr.get_discount(), None)
 
     def test_two_voucher_items(self) -> None:
         """Test if the voucher discount is applied for two voucher items"""
+        
+        products: List[Product] = self.voucher_list_generator(2)
+        for p in products:
+            self.vr.scan()
 
-        pass
+        discount: Discount = self.vr.get_discount()
+
+        self.assertAlmostEqual(discount.discount, 5.0)
 
     def test_three_voucher_items(self) -> None:
         """Test if the voucher discount is applied for three voucher items"""
-        pass
+        
+        products: List[Product] = self.voucher_list_generator(3)
+        for p in products:
+            self.vr.scan()
+
+        discount: Discount = self.vr.get_discount()
+
+        self.assertAlmostEqual(discount.discount, 5.0)
 
 
 if __name__ == '__main__':
