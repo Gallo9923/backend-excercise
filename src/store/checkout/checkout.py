5ff577c6f2e4f83c80d1a82d26b0bad31a35cc52
Christian Gallo <42328477+Gallo9923@users.noreply.github.com>
2022-07-31 17:58:22 -0500

    Add Checkout class skeleton code

---
+++ src/store/checkout/checkout.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import List
+
+from store.checkout.pricing_rule import PricingRule
+from store.checkout.discount_calculator import DiscountCalculator
+from store.checkout.calculator import Calculator
+from store.checkout.product import Product
+
+@dataclass
+class Checkout():
+    """Calculates the total price of a checkout of products with discounts"""
+
+    pricing_rules: List[PricingRule]
+    discount_calculator: DiscountCalculator
+    calculator: Calculator
+
+    def scan(self, product: Product) -> None:
+        pass
+
+    def get_total(self) -> float:
+        pass
