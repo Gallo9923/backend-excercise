7775daeee87fee77604b94a6e2d05c53ffb82c69
Christian Gallo <42328477+Gallo9923@users.noreply.github.com>
2022-07-31 14:27:19 -0500

    Add discount calculator tests

--- src/store/checkout/discount_calculator.py
+++ src/store/checkout/discount_calculator.py
@@ -1,13 +1,12 @@
-
-
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import List
 from store.checkout.discount import Discount
 from store.checkout.pricing_rule import PricingRule
 
 from store.checkout.product import Product
 
-
+@dataclass
 class DiscountCalculator(ABC):
 
     pricing_rules: List[PricingRule]
@@ -20,11 +19,11 @@ class DiscountCalculator(ABC):
     def get_discounts(self) -> List[Discount]:
         pass
 
-
+@dataclass
 class DiscountCalculatorImpl(DiscountCalculator):
-
+    
     def scan(self, product: Product) -> None:
         pass
     
     def get_discounts(self) -> List[Discount]:
-        pass
+        return []
